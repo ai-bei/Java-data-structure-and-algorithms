c462f39054b48fffa608492dcc23ab28b0eae81a
Annicko <18296768912@139.com>
2020-04-20 17:13:12 +0800

    递归算法和回溯算法 应用实例 1、两点间路线规划 2、八皇后求解

---
+++ Algorithm/src/main/java/com/iotknowyou/Recursion/FindWay.java
@@ -0,0 +1,123 @@
+package com.iotknowyou.Recursion;
+
+/**
+ * @author LiuRongHua
+ * @updateUser
+ * @description  使用递归回溯算法，求路线规划的问题
+ * @updateRemark
+ * @createDate 2020/4/20 11:54
+ * @updateDate 2020/4/20 11:54
+ **/
+public class FindWay {
+
+    // 地图数据
+    private static  int[][] map ;
+    private static  int mapRow ;
+    private static  int mapColumn ;
+
+
+    /**
+     *  初始化地图
+     * @param row
+     * @param column
+     * @return
+     */
+    public static int[][] initMap(int row , int column){
+        // 先创建一个二维数组
+        // 模拟地图
+         map = new int[row][column];
+         mapRow = row ;
+         mapColumn = column ;
+        // 使用1 表示墙
+        // 上下全部置为1
+        for (int i = 0; i < column  ; i++) {
+            map[0][i] = 1;
+            map[row - 1][i] = 1;
+        }
+        // 左右全部置为1
+        for (int i = 0; i < row  ; i++) {
+            map[i][0] = 1;
+            map[i][column - 1] = 1;
+        }
+        //设置挡板, 1 表示
+        setBlock(3,1,1);
+        setBlock(3,2,1);
+        return map;
+    }
+
+    /**
+     * 打印地图
+     */
+    public static void printMap(){
+        // 输出地图
+        System.out.println("地图的情况");
+        for (int i = 0; i < mapRow; i++) {
+            for (int j = 0; j < mapColumn ; j++) {
+                System.out.print(map[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    /**
+     *
+     * @param row
+     * @param column
+     * @param mark     当为 1 表示墙  ； 2 表示通路可以走 ； 3 表示该点已经走过，但是走不通
+     */
+    public static void setBlock(int row , int column ,int mark){
+        if(row > mapRow ){
+            throw new RuntimeException("输入的row = "+row +", 大于最大值"+mapRow);
+        }
+        if(column > mapColumn){
+            throw new RuntimeException("输入的row = "+ column +", 大于最大值"+mapColumn);
+        }
+        map[row][column] = mark;
+    }
+
+
+    /**
+     *
+     * @param startRow      起点
+     * @param startColumn   起点
+     * @param endRow        终点
+     * @param endColumn     终点
+     * @return  策略(方法) 下->右->上->左 , 如果该点走不通，再回溯
+     *                    选择的策略不同，最终的结果也会不同
+     */
+    public static boolean setWay( int startRow, int startColumn , int endRow, int endColumn ) {
+        // TODO 对输入的参数进行校验
+        if(map[endRow][endColumn] == 2) { // 通路已经找到ok
+            return true;
+        } else {
+            if(map[startRow][startColumn] == 0) { //如果当前这个点还没有走过
+                //按照策略 下->右->上->左  走
+                setBlock(startRow,startColumn,2); // 假定该点是可以走通.
+                if(setWay(startRow + 1, startColumn ,endRow , endColumn)) {//向下走
+                    return true;
+                } else if (setWay(startRow, startColumn + 1 ,endRow , endColumn)) { //向右走
+                    return true;
+                } else if (setWay(startRow - 1, startColumn ,endRow , endColumn)) { //向上
+                    return true;
+                } else if (setWay(startRow, startColumn - 1 ,endRow , endColumn)){ // 向左走
+                    return true;
+                } else {
+                    //说明该点是走不通，是死路
+                    setBlock(startRow,startColumn,3);
+                    return false;
+                }
+            } else { // 如果map[i][j] != 0 , 可能是 1， 2， 3
+                return false;
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        initMap(8,7);
+        printMap();
+        setWay(3,5,6,5);
+        printMap();
+
+    }
+}
---
+++ Algorithm/src/main/java/com/iotknowyou/Recursion/Queen8.java
@@ -0,0 +1,100 @@
+package com.iotknowyou.Recursion;
+
+/**
+ * @author LiuRongHua
+ * @updateUser
+ * @description  使用递归回溯，求解  八皇后的问题
+ * @updateRemark
+ * @createDate 2020/4/20 14:10
+ * @updateDate 2020/4/20 14:10
+ **/
+public class Queen8 {
+    /*
+    *  问题的描述：
+    *       八皇后问题，是一个古老而著名的问题，是回溯算法的典型案例。
+    *       该问题是国际西洋棋棋手马克斯·贝瑟尔于1848年提出：在8×8格的国际象棋上摆放八个皇后，
+    *       使其不能互相攻击，即：任意两个皇后都不能处于同一行、同一列或同一斜线上，问有多少种摆法。
+    *
+    * */
+
+
+    /**
+     *      1) 第一个皇后先放第一行第一列
+     *      2) 第二个皇后放在第二行第一列、然后判断是否OK[即判断是冲突]， 如果不OK，继续放在第二列、第三列、依次把所有列都放完，找到一个合适
+     *      3) 继续第三个皇后，还是第一列、第二列……直到第8个皇后也能放在一个不冲突的位置，算是找到了一个正确解
+     *      4) 当得到一个正确解时，在栈回退到上一个栈时，就会开始回溯，即将第一个皇后，放到第一列的所有正确解，全部得到.
+     *      5) 然后回头继续第一个皇后放第二列，后面继续循环执行 1,2,3,4的步骤
+     */
+
+
+    //定义一个max表示共有多少个皇后
+    int max = 8;
+    //定义数组array, 保存皇后放置位置的结果,比如 arr = {0 , 4, 7, 5, 2, 6, 1, 3}
+    int[] array = new int[max];
+    static int count = 0;
+    static int judgeCount = 0;
+    public static void main(String[] args) {
+        //测试一把 ， 8皇后是否正确
+        Queen8 queen8 = new Queen8();
+        queen8.check(0);
+        System.out.printf("一共有%d解法", count);
+        System.out.printf("一共判断冲突的次数%d次", judgeCount); // 1.5w
+
+    }
+
+
+
+    //编写一个方法，放置第n个皇后
+    //特别注意： check 是 每一次递归时，进入到check中都有  for(int i = 0; i < max; i++)，因此会有回溯
+    // 类比  8个嵌套的 for循环
+    private void check(int n) {
+        if(n == max) {  //n = 8 , 其实8个皇后就既然放好
+            print();
+            return;
+        }
+
+        //依次放入皇后，并判断是否冲突
+        for(int i = 0; i < max; i++) {
+            //先把当前这个皇后 n , 放到该行的第1列
+            array[n] = i;
+            //判断当放置第n个皇后到i列时，是否冲突
+            if(judge(n)) { // 不冲突
+                //接着放n+1个皇后,即开始递归
+                check(n+1); //
+            }
+            //如果冲突，就继续执行 array[n] = i; 即将第n个皇后，放置在本行得 后移的一个位置
+        }
+    }
+
+    //查看当我们放置第n个皇后, 就去检测该皇后是否和前面已经摆放的皇后冲突
+    /**
+     *
+     * @param n 表示第n个皇后
+     * @return
+     */
+    private boolean judge(int n) {
+        judgeCount++;
+        for(int i = 0; i < n; i++) {
+            // 说明
+            //1. array[i] == array[n]  表示判断 第n个皇后是否和前面的n-1个皇后在同一列
+            //2. Math.abs(n-i) == Math.abs(array[n] - array[i]) 表示判断第n个皇后是否和第i皇后是否在同一斜线
+            // n = 1  放置第 2列 1 n = 1 array[1] = 1
+            // Math.abs(1-0) == 1  Math.abs(array[n] - array[i]) = Math.abs(1-0) = 1
+            //3. 判断是否在同一行, 没有必要，n 每次都在递增
+            if(array[i] == array[n] || Math.abs(n-i) == Math.abs(array[n] - array[i]) ) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    //写一个方法，可以将皇后摆放的位置输出
+    private void print() {
+        count++;
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+
+}
